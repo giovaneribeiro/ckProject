c49d9baf844ebb908226005d335b31d2abce875e
Giovane Ribeiro <gar018@hotmail.com>
2016-04-21 18:45:18 -0300

    Adicionado opcao de remover e log de acoes

--- js/nodes.js
+++ js/nodes.js
@@ -1,22 +1,27 @@
-/* Informações dos círculos */
+/* Atributos do circulo */
 var raio = 50;
 
+/* Informacoes */
 var nodeInfo = $('#nodeInfo');
-var info = "Selecione um nós para ver suas informações.";
+var info = "Selecione um dos nós para ver suas informações.";
 nodeInfo.append(info);
 
+/* Grupo de nos */
 var nodes = [];
 var nodeGroup = $('#nodeGroup');
 
+/* HEAD */
 var head = createNode("HEAD");
 nodeGroup.append(head);
 
+/* Visualizar informacoes */
 function showNodeInfo(circle, event) {
     nodeInfo.empty();
 
-    var position = searchNode(circle.textContent);
+    var textContent = circle.textContent.substr(0, circle.textContent.length-1);
+    var position = searchNode(textContent);
 
-    nodeInfo.append('Valor: ' + circle.textContent);
+    nodeInfo.append('Valor: ' + textContent);
     nodeInfo.append('<br>Posição: ' + (position==-1?'-':position));
 
     if(position+1 == nodes.length) {
@@ -27,21 +32,30 @@ function showNodeInfo(circle, event) {
     }
 }
 
+/* Esconder informacoes */
 function hideNodeInfo() {
     nodeInfo.empty();
     nodeInfo.append(info);
 }
 
+/* Criar SVG */
 function createNode(value) {
 
     return '<svg width="'+ raio*2 +'" height="'+ raio*2 +'" class="circle" onmouseover="showNodeInfo(this, event)" onmouseout="hideNodeInfo()">' +
+
         '<circle cx="'+ raio +'" cy="'+ raio +'" r="'+ raio +'"></circle>' +
+        '<circle cx="15" cy="15" r="15" class="remove" onclick="removeNode(this)"></circle>' +
+
         '<text x="'+ raio +'" y="'+ raio +'">' +
-        '<tspan>' + value + '</tspan>' +
+            '<tspan>' + value + '</tspan>' +
         '</text>' +
+
+        '<text x="15" y="15" onclick="removeNode(this)">x</text>' +
+
         '</svg> ';
 }
 
+/* Adicionar */
 function addNode() {
     var nodeName = $('#nodeName').val();
 
@@ -60,8 +74,37 @@ function addNode() {
     nodes.push({"value": nodeName});
 
     $('#nodeName').val('');
+
+    $('#log').append(log('Adicionar', nodeName, nodes.length-1));
 }
 
+/* Remover */
+function removeNode(node) {
+
+    var svg = node.parentNode;
+
+    var textContent = svg.textContent.substr(0, svg.textContent.length-1);
+
+    if(textContent == 'HEAD') {
+        nodeGroup.empty();
+        nodeGroup.append(head);
+
+        $('#log').append(log('Zerar', '-', '-'));
+
+        return ;
+    }
+
+    svg.parentNode.removeChild(svg);
+
+    var position = searchNode(textContent);
+    nodes.splice(position, 1);
+
+    hideNodeInfo();
+
+    $('#log').append(log('Remover', textContent, position));
+}
+
+/* Procurar */
 function searchNode(value) {
 
     if(value == 'HEAD') return -1;
@@ -70,4 +113,14 @@ function searchNode(value) {
         if(nodes[i].value == value)
             return i;
     }
+}
+
+/* Criar linha do Log */
+function log(action, value, position) {
+
+    return '<tr>' +
+        '<td>'+ action +'</td>' +
+        '<td>'+ value +'</td>' +
+        '<td>'+ position +'</td>' +
+        '</tr>';
 }
